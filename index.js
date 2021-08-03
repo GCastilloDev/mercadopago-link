5c397286090cd2110acf5478abce934798a78b01
Gustavo Castillo N <gcnctz@gmail.com>
2021-08-03 16:18:26 -0500

    INTEGRACION FIREBASE

--- index.js
+++ index.js
@@ -1,49 +1,72 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 
-
-const mercadopago = require('mercadopago');
 const PORT = process.env.PORT;
+const mercadopago = require('mercadopago');
+
+const admin = require('firebase-admin');
+const serviceAccount = require('./src/cmas-321818-965b6cb73461.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/woke-up', (req, res) => {
+  res.send({ message: 'Thanks i woke up!' });
+});
 
-app.get("/prueba", (req, res) => {
-    res.send({ message: 'OK' });
-})
+app.get('/status-mercado-pago', async (req, res) => {
+  const url =
+    'https://api.mercadopago.com/checkout/preferences/315731691-6fb02e83-4815-47ab-a131-c67288604833';
+  // 'https://api.mercadopago.com/v1/payments/315731691-6fb02e83-4815-47ab-a131-c67288604833';
 
-app.post('/', (req, res) => {
+  const headers = {
+    Authorization: `Bearer ${process.env.MERCADO_PAGO_TOKEN}`,
+  };
 
-    console.log(req.body);
+  const { data } = await axios.get(url, { headers });
+  console.log(data);
+  res.send(data);
+});
 
-    mercadopago.configure({
-        access_token: process.env.MERCADO_PAGO_TOKEN
-    });
-    
-    let preference = {
+app.post('/', async (req, res) => {
+  const idFirebase = req.body.idFirebase;
+
+  mercadopago.configure({
+    access_token: process.env.MERCADO_PAGO_TOKEN,
+  });
+
+  let preference = {
     items: req.body.items,
     payer: req.body.user,
     back_urls: {
-        failure: process.env.HOST_FRONT,
-        pending: process.env.HOST_FRONT,
-        success: process.env.HOST_FRONT,
+      failure: process.env.HOST_FRONT,
+      pending: process.env.HOST_FRONT,
+      success: process.env.HOST_FRONT,
     },
-    auto_return: "approved",
-    };
+    auto_return: 'approved',
+  };
 
-    mercadopago.preferences.create(preference)
-    .then(function({response}){
-    const linkDePago =  response.init_point;
-    res.send({linkDePago})
-    }).catch(function (error) {
+  try {
+    const { response } = await mercadopago.preferences.create(preference);
+    const idMercadoPago = response.id;
+
+    await db.collection('orders').doc(idFirebase).update({ idMercadoPago });
+
+    linkDePago = response.init_point;
+    res.send({ linkDePago });
+  } catch (error) {
     console.log(error);
-    });
-})
-
+  }
+});
 
 app.listen(PORT, () => {
-    console.log("SERVIDOR INICIADO EN PUERTO", PORT);
-})
+  console.log('SERVIDOR INICIADO EN PUERTO', PORT);
+});
